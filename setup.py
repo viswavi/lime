ae70a2f6a3438fe434348b220e516ab1406c3384
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-05-03 18:05:36 -0400

    Update setup.py to use comparelime package name

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='lime',
+setup(name='comparelime',
       version='0.2.0.1',
       description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
-      url='http://github.com/marcotcr/lime',
-      author='Marco Tulio Ribeiro',
-      author_email='marcotcr@gmail.com',
+      url='http://github.com/viswavi/lime',
       license='BSD',
       packages=find_packages(exclude=['js', 'node_modules', 'tests']),
       python_requires='>=3.5',
