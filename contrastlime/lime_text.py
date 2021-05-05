61045aa385fe7a89c6cad21ab890ea153922526e
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-05-04 21:34:04 -0400

    Implement __data_labels_distances_contrast [wip]

--- contrastlime/lime_text.py
+++ contrastlime/lime_text.py
@@ -433,6 +433,77 @@ class LimeTextExplainer(object):
                 feature_selection=self.feature_selection)
         return ret_exp
 
+    def explain_instance_compare(self,
+                         text_instance,
+                         classifier_fn_a,
+                         classifier_fn_b,
+                         labels=(1,),
+                         top_labels=None,
+                         num_features=10,
+                         num_samples=5000,
+                         label_style="classification",
+                         distance_metric='cosine',
+                         model_regressor=None):
+        """Generates explanations for a prediction.
+
+        First, we generate neighborhood data by randomly hiding features from
+        the instance (see __data_labels_distance_mapping). We then learn
+        locally weighted linear models on this neighborhood data to explain
+        each of the classes in an interpretable way (see lime_base.py).
+
+        Args:
+            text_instance: raw text string to be explained.
+            classifier_fn_a: One classifier prediction probability function, which
+                takes a list of d strings and outputs a (d, k) numpy array with
+                prediction probabilities, where k is the number of classes.
+                For ScikitClassifiers , this is classifier.predict_proba.
+            classifier_fn_b: Another classifier prediction probability function.
+            labels: iterable with labels to be explained.
+            top_labels: if not None, ignore labels and produce explanations for
+                the K labels with highest prediction probabilities, where K is
+                this parameter.
+            num_features: maximum number of features present in explanation
+            num_samples: size of the neighborhood to learn the linear model
+            distance_metric: the distance metric to use for sample weighting,
+                defaults to cosine similarity
+            model_regressor: sklearn regressor to use in explanation. Defaults
+            to Ridge regression in LimeBase. Must have model_regressor.coef_
+            and 'sample_weight' as a parameter to model_regressor.fit()
+        Returns:
+            An Explanation object (see explanation.py) with the corresponding
+            explanations.
+        """
+
+        indexed_string = (IndexedCharacters(
+            text_instance, bow=self.bow, mask_string=self.mask_string)
+                          if self.char_level else
+                          IndexedString(text_instance, bow=self.bow,
+                                        split_expression=self.split_expression,
+                                        mask_string=self.mask_string))
+        domain_mapper = TextDomainMapper(indexed_string)
+        data, yss, distances = self.__data_labels_distances_contrast(
+            indexed_string, classifier_fn_a, classifier_fn_b, num_samples,
+            distance_metric=distance_metric, label_style=label_style)
+        if self.class_names is None:
+            self.class_names = [str(x) for x in range(yss[0].shape[0])]
+        ret_exp = explanation.Explanation(domain_mapper=domain_mapper,
+                                          class_names=self.class_names,
+                                          random_state=self.random_state)
+        ret_exp.predict_proba = yss[0]
+        if top_labels:
+            labels = np.argsort(yss[0])[-top_labels:]
+            ret_exp.top_labels = list(labels)
+            ret_exp.top_labels.reverse()
+        for label in labels:
+            (ret_exp.intercept[label],
+             ret_exp.local_exp[label],
+             ret_exp.score[label],
+             ret_exp.local_pred[label]) = self.base.explain_instance_with_data(
+                data, yss, distances, label, num_features,
+                model_regressor=model_regressor,
+                feature_selection=self.feature_selection)
+        return ret_exp
+
     def __data_labels_distances(self,
                                 indexed_string,
                                 classifier_fn,
@@ -481,5 +552,78 @@ class LimeTextExplainer(object):
             data[i, inactive] = 0
             inverse_data.append(indexed_string.inverse_removing(inactive))
         labels = classifier_fn(inverse_data)
+        distances = distance_fn(sp.sparse.csr_matrix(data))
+        return data, labels, distances
+
+    def __data_labels_distances_contrast(self,
+                                indexed_string,
+                                classifier_fn_a,
+                                classifier_fn_b,
+                                num_samples,
+                                baseline_label=1,
+                                distance_metric='cosine',
+                                label_style="classification"):
+        """Generates a neighborhood around a prediction.
+
+        Generates neighborhood data by randomly removing words from
+        the instance, and predicting with the classifier. Uses cosine distance
+        to compute distances between original and perturbed instances.
+        Args:
+            indexed_string: document (IndexedString) to be explained,
+            classifier_fn_a: One classifier prediction probability function, which
+                takes a string and outputs prediction probabilities. For
+                ScikitClassifier, this is classifier.predict_proba.
+            indexed_string: document (IndexedString) to be explained,
+            classifier_fn_b: Another classifier prediction probability function.
+            num_samples: size of the neighborhood to learn the linear model
+            baseline_label: label value whose predicted probability we compare
+                            between models.
+            distance_metric: the distance metric to use for sample weighting,
+                defaults to cosine similarity.
+
+
+        Returns:
+            A tuple (data, labels, distances), where:
+                data: dense num_samples * K binary matrix, where K is the
+                    number of tokens in indexed_string. The first row is the
+                    original instance, and thus a row of ones.
+                labels: num_samples * L matrix, where L is the number of target
+                    labels
+                distances: cosine distance between the original instance and
+                    each perturbed instance (computed in the binary 'data'
+                    matrix), times 100.
+        """
+        def distance_fn(x):
+            return sklearn.metrics.pairwise.pairwise_distances(
+                x, x[0], metric=distance_metric).ravel() * 100
+
+        doc_size = indexed_string.num_words()
+        sample = self.random_state.randint(1, doc_size + 1, num_samples - 1)
+        data = np.ones((num_samples, doc_size))
+        data[0] = np.ones(doc_size)
+        features_range = range(doc_size)
+        inverse_data = [indexed_string.raw_string()]
+        for i, size in enumerate(sample, start=1):
+            inactive = self.random_state.choice(features_range, size,
+                                                replace=False)
+            data[i, inactive] = 0
+            inverse_data.append(indexed_string.inverse_removing(inactive))
+        pred_probs_a = classifier_fn_a(inverse_data)
+        pred_probs_b = classifier_fn_b(inverse_data)
+        assert pred_probs_a.shape == pred_probs_b.shape
+        prediction_difference = (pred_probs_b - pred_probs_a)[:, baseline_label]
+        # Make the "predicted probability" matrix have two columns: the difference between model A and model B for 
+        # a desired label, and the difference between model B and model A for the desired label.
+
+        if label_style == "classification":
+            contrast_matrix = np.zeros(pred_probs_a.shape)
+            contrast_matrix[:,1] = prediction_difference
+            contrast_matrix[:,0] = -prediction_difference
+            labels = contrast_matrix
+        elif label_style == "regression":
+            labels = prediction_difference
+        else:
+            raise ValueError("Compare-lime must be done via either classification or regression.")
+
         distances = distance_fn(sp.sparse.csr_matrix(data))
         return data, labels, distances
