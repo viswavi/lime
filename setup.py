82ec3778cd5bc625b44b86deb5ef85940a312edb
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-05-05 01:08:33 -0400

    Rename comparelime to contrastlime

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='comparelime',
+setup(name='contrastlime',
       version='0.2.0.1',
       description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
       url='http://github.com/viswavi/lime',
